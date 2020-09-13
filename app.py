2461c6d2d020e016a086ab5db1cd7f6033b93a67
Darren Bridges <darrenbridges@Darrens-Air.lan>
2020-09-13 15:06:46 +0100

    with html template

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 # A welcome message to test our server
 @app.route('/')
 def index():
-    return "<h1>Welcome!</h1>"
+    return render_template("home.html", title="Heroku Test App")
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
